477512ee940e8050420917d35f912924fc288950
Christoph Otter <christophotter@gmail.com>
2016-10-25 11:28:29 +0200

    Added macro generator

---
+++ generators/macro/index.js
@@ -0,0 +1,68 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var fileHelper = require('../filehelper');
+var helper = require('../helper');
+
+var cwd = process.cwd();
+
+module.exports = yeoman.Base.extend({
+    constructor: function () {
+        yeoman.Base.apply(this, arguments);
+
+        fileHelper.registerPackageOption(this);
+    },
+    initializing: function () {
+        this.destinationRoot(cwd);
+    },
+    prompting: function () {
+        var prompts = [{
+            type: 'input',
+            name: 'macroNames',
+            validate: fileHelper.validateCommaTypeList,
+            message: 'List macro names (separated by commas):\n'
+        }];
+        fileHelper.addCurrentPackagePrompt(this, prompts);
+
+        return this.prompt(prompts).then(function (values) {
+            this.props = values;
+            this.files = [];
+
+            helper.iterateCommaList(values.macroNames, function (macroName) {
+                if (macroName === '')
+                    return;
+
+                var parts = macroName.split('.');
+                var name = parts.pop();
+                var pack = parts.join('.');
+
+                if (!name.endsWith('Macro'))
+                    name += 'Macro';
+
+                var fullPack = helper.joinIfNotEmpty([this.options.currentPackage, pack], '.');
+
+                var file = {
+                    name: name,
+                    package: pack,
+                    fullPackage: fullPack
+                };
+
+                this.files.push(file);
+            }.bind(this));
+        }.bind(this));
+    },
+    writing: function () {
+        for (var file of this.files) {
+            var scope = {
+                author: this.user.git.name(),
+                package: file.fullPackage,
+                name: file.name
+            };
+
+            var files = new Map([
+                ['Macro.hx', file.name]
+            ]);
+
+            fileHelper.writeFilesToPackage(this, files, file.package, scope);
+        }
+    }
+});
---
+++ generators/macro/templates/Macro.hx
@@ -0,0 +1,14 @@
+package <%= package %>;
+import hex.control.macro;
+
+/**
+ * Generated code
+ * @author <%= author %>
+ */
+class <%= name %> extends Macro
+{
+	override function _prepare() : Void
+	{
+		//this.add( MockCommand );
+	}
+}
