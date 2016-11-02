015e119a3916f4bc5c9a0573090c26a8489ef76e
Christoph Otter <christophotter@gmail.com>
2016-11-02 22:22:24 +0100

    Further work on service generator

--- generators/service/index.js
+++ generators/service/index.js
@@ -20,31 +20,14 @@ module.exports = yeoman.Base.extend({
             name: 'serviceNames',
             validate: fileHelper.validateCommaTypeList,
             message: 'List service names (separated by commas):\n'
-        }, {
-            type: 'list',
-            name: 'serviceType',
-            message: 'Please choose a service type:\n',
-            choices: [
-                'HttpService',
-                'Stateful',
-                'AsyncStateless'
-            ]
-        }, {
-            type: 'confirm',
-            name: 'createInterface',
-            message: 'Do you want to implement an interface?',
-            default: false
-        }, {
-            type: 'input',
-            name: 'interfaceName',
-            validate: fileHelper.validateHaxeType,
-            message: 'Please enter an interface name:\n'
         }];
         fileHelper.addCurrentPackagePrompt(this, prompts);
 
         return this.prompt(prompts).then(function (values) {
             this.props = values;
             this.files = [];
+
+            var promise = null;
 
             helper.iterateCommaList(values.serviceNames, function (serviceName) {
                 if (serviceName === '')
@@ -53,24 +36,69 @@ module.exports = yeoman.Base.extend({
                 var parts = serviceName.split('.');
                 var name = parts.pop();
                 var pack = parts.join('.');
-                var endPack = 'service.' + name.toLowerCase();
+                var endPack = 'service.' + name.toLowerCase(); //TODO: make this a little smarter
 
                 if (!this.options.currentPackage.endsWith(endPack) && !pack.startsWith(endPack))
                     pack = helper.joinIfNotEmpty([endPack, pack], '.');
 
                 var fullPack = helper.joinIfNotEmpty([this.options.currentPackage, pack], '.');
 
-                var file = {
-                    name: name,
-                    package: pack,
-                    fullPackage: fullPack,
-                    Service: name,
-                    serviceType: this.props.serviceType,
-                    hasInterface: values.createInterface
-                };
+                var prompts = [{
+                    type: 'list',
+                    name: 'serviceType',
+                    message: 'Please choose a service type for ' + serviceName + ':\n',
+                    choices: [
+                        'Http',
+                        'Stateful',
+                        'AsyncStateless'
+                    ]
+                }, {
+                    type: 'confirm',
+                    name: 'createInterface',
+                    message: 'Do you want to implement an interface?',
+                    default: false
+                }, {
+                    type: 'input',
+                    name: 'interfaceName',
+                    validate: function (value) {
+                        if (!fileHelper.validateHaxeType(value)) {
+                            return 'Not a valid interface name: "' + value + '"';
+                        }
+                        return true;
+                    },
+                    message: 'Please enter an interface name:\n',
+                    when: function (values) {
+                        return values.createInterface;
+                    }
+                }, {
+                    type: 'confirm',
+                    name: 'serviceParser',
+                    message: 'Do you want to create a parser?',
+                    default: true,
+                    when: function (values) {
+                        return values.serviceType === 'Http';
+                    }
+                }];
 
-                this.files.push(file);
+                promise = helper.chainPrompts(this, promise, prompts, function (values) {
+                    //this.composeWith('hex:') //TODO: create HttpParser generator
+
+                    var file = {
+                        name: name,
+                        package: pack,
+                        fullPackage: fullPack,
+                        Service: name,
+                        serviceType: values.serviceType,
+                        hasInterface: values.createInterface,
+                        //serviceParser: values.serviceParser,
+                        IService: values.interfaceName
+                    };
+
+                    this.files.push(file);
+                }.bind(this));
             }.bind(this));
+
+            return promise;
         }.bind(this));
     },
     writing: function () {
@@ -78,8 +106,7 @@ module.exports = yeoman.Base.extend({
             var scope = {
                 author: this.user.git.name(),
                 package: file.fullPackage,
-                service: file,
-                createInterface: file.hasInterface
+                service: file
             };
 
             var files = [];
@@ -88,10 +115,14 @@ module.exports = yeoman.Base.extend({
                 files.push(['StatefulService.hx', file.Service]);
             else if (file.serviceType === 'AsyncStateless')
                 files.push(['AsyncStateless.hx', file.Service]);
-            else if (files.serviceType === 'HttpService')
+            else if (file.serviceType === 'Http')
                 files.push(['HttpService.hx', file.Service]);
+
+            this.log(files);
 
             fileHelper.writeFilesToPackage(this, new Map(files), file.package, scope);
         }
+
+
     }
 });
--- generators/service/templates/StatefulService.hx
+++ generators/service/templates/StatefulService.hx
@@ -6,7 +6,7 @@ import hex.service.stateful.StatefulService;
  * Generated code
  * @author <%= author %>
  */
-class <%= service.Service %> extends StatefulService
+class <%= service.Service %> extends StatefulService<% if (service.hasInterface) { %> implements <%= service.IService %><% } %>
 {
 	public function new()
 	{
